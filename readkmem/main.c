2f3114ea9e0e0a2d9a9f9cbeea4a850c1789e47b
Pedro Hugo <pedrohugo@whq.(none)>
2011-09-22 22:00:35 +0100

    Fixes and improvements!

--- readkmem/main.c
+++ readkmem/main.c
@@ -11,6 +11,8 @@
  * gcc -Wall -o readkmem readkmem.c
  *
  * v0.1 - Initial version
+ * v0.2 - Some fixes
+ * v0.3 - More improvements, more useful now!
  *
  */
 #include <stdio.h>
@@ -22,14 +24,16 @@
 #include <string.h>
 #include <stdint.h>
 #include <sys/sysctl.h>
+#include <getopt.h>
+#include <ctype.h>
 
 #define DEBUG 1
-#define VERSION "0.1"
+#define VERSION "0.3"
 
 #define x86 0
 #define x64	1
 
-#define MAX_SIZE 5000
+#define MAX_SIZE 500000
 
 /***********GLOBAL**************/
 int fd_kmem;
@@ -55,7 +59,7 @@ int8_t get_kernel_type (void)
 		return -1;
 }
 
-void readkmem(void *m,off_t off,int size)
+void readkmem(void *m,off_t off,uint64_t size)
 {
 	if(lseek(fd_kmem,off,SEEK_SET) != off)
 	{
@@ -69,7 +73,7 @@ void readkmem(void *m,off_t off,int size)
 	}
 }
 
-void writekmem(void *m,off_t off,int size)
+void writekmem(void *m,off_t off,uint64_t size)
 {
 	if(lseek(fd_kmem,off,SEEK_SET) != off)
 	{
@@ -85,8 +89,9 @@ void writekmem(void *m,off_t off,int size)
 
 void usage(void)
 {
+	fprintf(stderr,"readkmem -a address -s size [-out filename]\n");
 	fprintf(stderr,"Available Options : \n");
-	fprintf(stderr,"       -a nb    show all info about one interrupt\n");
+	fprintf(stderr,"       -out filename	file to write binary output to\n");
 	exit(1);
 }
 
@@ -99,47 +104,135 @@ void header(void)
 int main(int argc, char ** argv)
 {
     
+	// required structure for long options
+	static struct option long_options[]={
+		{ "address", required_argument, NULL, 'a' },
+		{ "size", required_argument, NULL, 's' },
+		{ "out", required_argument, NULL, 'o' },
+		{ NULL, 0, NULL, 0 }
+	};
+	int option_index = 0, c;
+	char *outputname=NULL;
+	
+	uint64_t address=0, size=0;
+	
+	// process command line options
+	while ((c = getopt_long (argc, argv, "a:s:o:", long_options, &option_index)) != -1)
+	{
+		switch (c)
+		{
+			case ':':
+				usage();
+				exit(1);
+				break;
+			case '?':
+				usage();
+				exit(1);
+				break;
+			case 'o':
+				outputname = optarg;
+				break;
+			case 'a':
+				address = strtoul(optarg, NULL, 0);
+				break;
+			case 's':
+				size = strtoul(optarg, NULL, 0);
+				break;
+			default:
+				usage();
+				exit(1);
+		}
+	}
+	
 	header();
+
 	if (argc < 3)
 	{
 		usage();
+	}
+	
+	// we need to run this as root
+	if (getuid() != 0)
+	{
+		printf("[ERROR] Please run me as root!\n");
+		exit(1);
 	}
 	
 	kernel_type = get_kernel_type();
 	if (kernel_type == -1)
 	{
 		printf("[ERROR] Unable to retrieve kernel type!\n");
-		exit(-1);
+		exit(1);
 	}
 	
 	if(!(fd_kmem=open("/dev/kmem",O_RDWR)))
 	{
 		fprintf(stderr,"[ERROR] Error while opening /dev/kmem. Is /dev/kmem enabled?\n");
-		exit(-1);
+		fprintf(stderr,"Add parameter kmem=1 to /Library/Preferences/SystemConfiguration/com.apple.Boot.plist\n");
+		exit(1);
 	}
 	
-    unsigned int address, size;
-    size = atoi(argv[2]);
     if (size > MAX_SIZE)
     {
         printf("[ERROR] Invalid size (higher than maximum!)\n");
         exit(1);
     }
     unsigned char *read = malloc(size);
-    if (read == NULL)
+	if (read == NULL)
     {
         printf("[ERROR] Memory allocation failed!\n");
         exit(1);
     }
-    sscanf(argv[1],"%x",&address);
-
+	
+	FILE *outputfile;
+	if (outputname != NULL)
+	{
+		
+		if ( (outputfile = fopen(outputname, "wb")) == NULL)
+		{
+			fprintf(stderr,"[ERROR] Cannot open %s for output!\n", outputname);
+			exit(1);
+		}
+	}
+	
+	// read kernel memory
     readkmem(read, address, size);
+	
+	if (outputname != NULL)
+	{
+		if (fwrite(read, size, 1, outputfile) < 1)
+		{
+			fprintf(stderr,"[ERROR] Write error at %s occurred!\n", outputname);
+			exit(1);
+		}
+		printf("\n[OK] Memory dumped to %s!\n\n", outputname);
+	}
+	else
+	{
+		int i=0,x,z;
+		printf("Memory hex dump @ %p:\n\n", (void*)address);
+		// 16 columns
+		while (i<size)
+		{
+			printf("%08lx ",address);
+			z = i;
+			for (x = 0; x < 16; x++)
+			{
+				printf("%02x ", read[z++]);
+			}
+			z = i;
+			for (x = 0; x < 16; x++)
+			{
+				printf("%c", isascii(read[z]) && isprint(read[z]) ? read[z] : '.');
+				z++;
+			}
+			i+=16;
+			printf("\n");
+			address += 16;
+		}
+		printf("\n");		
+	}
 
-    int i;
-    printf("Memory output @ 0x%x:\n", address);
-    for (i = 0; i < size; i++)
-        printf("%02x ", read[i]);
-    printf("\n");
     free(read);
 	return 0;
 }
